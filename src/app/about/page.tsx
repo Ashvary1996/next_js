59de4e8f9259f8024cebcdac6a309b375ece104e
Ashvary Gidian <ashvarygidian1996@gmail.com>
2024-11-16 16:11:09 +0530

    meta Data

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,9 @@
-import React from "react";
-
+export const metadata = {
+  title: "About CodeEvolution",
+  // description: "desc About CodeEvolution ",
+};
 function page() {
-  return <div>About</div>;
+  return <div>About me</div>;
 }
 
 export default page;
--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,12 +1,33 @@
-import React from "react";
+import { Metadata } from "next";
 
-function page({
-  params,
-}: {
+type Props = {
   params: {
     productId: string;
   };
-}) {
+};
+// dynamic meta-data
+// export const generateMetadata = ({ params }: Props): Metadata => {
+//   return {
+//     title: `Product ${params.productId}`,
+//   };
+// };
+
+// dynamic meta-data : for async
+export const generateMetadata = async ({
+  params,
+}: Props): Promise<Metadata> => {
+  const title = await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(`iPhone  ${params.productId}`);
+    }, 100);
+  });
+
+  return {
+    title: `Product ${title}`,
+  };
+};
+
+export default function ProductDetails({ params }: Props) {
   return (
     <>
       <div>Details about product : {params.productId}</div>
@@ -14,4 +35,16 @@ function page({
   );
 }
 
-export default page;
+// export default function ProductDetails({
+//   params,
+// }: {
+//   params: {
+//     productId: string;
+//   };
+// }) {
+//   return (
+//     <>
+//       <div>Details about product : {params.productId}</div>
+//     </>
+//   );
+// }
